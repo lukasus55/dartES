2e627d7f6af292702b48609a82a8e4efb62105ee
lukasus55 <lukas.kostyk@gmail.com>
2026-01-19 21:32:19 +0100

    Changed checkout guide color to customizablePrimary

--- app/components/CheckoutGuide.tsx
+++ app/components/CheckoutGuide.tsx
@@ -11,14 +11,14 @@ if (!guide) return null;
 
 return (
     <div className="flex flex-col items-center mt-2 w-full">
-        <span className="text-xs text-gray-400 mb-2 tracking-widest uppercase">
+        <span className="text-xs text-customizablePrimary mb-2 tracking-widest uppercase">
             Checkout Guide
         </span>
         <div className="flex gap-2 w-50">
             {guide.map((dart, i) => (
             <div 
                 key={i} 
-                className={`flex items-center justify-center px-3 py-1 rounded-md font-bold text-sm shadow-lg bg-customizableHighlit border-2 border-customizableAccent w-1/3`}
+                className={`flex items-center justify-center px-3 py-1 rounded-md font-bold text-sm text-customizablePrimary shadow-lg bg-customizableHighlit border-2 border-customizableAccent w-1/3`}
             >
                 {dart}
             </div>
