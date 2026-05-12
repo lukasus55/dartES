abbc8b02aa21efc23e08f53ba63bdb76ef9f3f78
lukasus55 <lukas.kostyk@gmail.com>
2026-05-12 23:12:28 +0200

    Add What's new page

---
+++ app/new/components/UpdateHeader.tsx
@@ -0,0 +1,5 @@
+export default function UpdateHeader({title}: {title: string}) {
+    return (
+        <div className="flex w-full justify-center text-4xl text-white font-semibold "> {title} </div>
+    )
+}
---
+++ app/new/components/UpdatesContainer.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import U_1_0_0 from "./Updates/U_1_0_0";
+
+export default function UpdatesContainer() {
+    return (
+        <div className="mt-10 flex gap-6">
+            <U_1_0_0 />
+
+        </div>
+    );
+}
