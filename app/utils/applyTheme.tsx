15e5bebe6bc98ef38642e282be56220549680ae9
lukasus55 <lukas.kostyk@gmail.com>
2026-05-13 00:10:08 +0200

    Fix ReferenceError: document is not defined

--- app/utils/applyTheme.tsx
+++ app/utils/applyTheme.tsx
@@ -4,6 +4,8 @@ import { getUserConfig } from "./configStorage";
 import { capitalizeFirstLetter } from "./helpers";
 
 export default function applyTheme() {
+    if (typeof document === "undefined") return;
+
     const config = getUserConfig();
     const theme = config.theme;
 
