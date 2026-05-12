7583372ed7e4ab04086b560260c2217c2848999c
lukasus55 <lukas.kostyk@gmail.com>
2026-05-12 21:43:01 +0200

    Improve MorePopup

--- app/components/MorePopup.tsx
+++ app/components/MorePopup.tsx
@@ -1,33 +1,43 @@
 "use client";
 
 import { useRef } from "react";
-import { TvMinimal, FileSpreadsheet } from "lucide-react";
+import { TvMinimal, FileSpreadsheet, Megaphone } from "lucide-react";
 import Link from "next/link";
 import { useClosePopup } from "@/app/utils/useClosePopup";
 import { exportMatchToExcel } from "../utils/exportMatchToExcel";
+import { useKeepInView } from "../utils/useKeepInView";
 
 export default function MorePopup({ onClose }: { onClose: () => void; }) {
     const popupRef = useRef<HTMLDivElement>(null);
     useClosePopup(popupRef, onClose);
+    useKeepInView(popupRef)
 
     return (
         <div
         ref={popupRef}
-        className="absolute left-3/4 flex flex-col gap-2 p-3 min-w-55
+        className="absolute left-3/4 flex flex-col gap-1 p-3 min-w-55
         bg-neutral-950 border-2 border-neutral-800 rounded-2xl shadow-2xl shadow-neutral-900/50
-        backdrop-blur-sm animate-in fade-in zoom-in-95 duration-200 z-50
+        backdrop-blur-sm animate-in fade-in zoom-in-95 duration-200 z-60
         max-h-[80vh] overflow-y-auto no-scrollbar"
         >
 
-            <Link href={"/setup"}>
-                <button className="flex items-center gap-2 hover:bg-neutral-800 rounded-sm px-2 cursor-pointer w-full">
-                    <TvMinimal width={14} /> Broadcast
+            <button className="flex items-center gap-2 hover:bg-neutral-800 rounded-sm px-2 py-1 cursor-pointer w-full" onClick={exportMatchToExcel}>
+                <FileSpreadsheet size={14} strokeWidth={2.5} /> Export to .xlsx
+            </button>
+
+            <div className="mt-1 mb-0.75 pb-px bg-neutral-700"></div>
+
+            <Link href={"/new"}>
+                <button className="flex items-center gap-2 hover:bg-neutral-800 rounded-sm px-2 py-1 cursor-pointer w-full">
+                    <Megaphone size={14} strokeWidth={2.5} /> What's New
                 </button>
             </Link>
 
-            <button className="flex items-center gap-2 hover:bg-neutral-800 rounded-sm px-2 cursor-pointer w-full" onClick={exportMatchToExcel}>
-                <FileSpreadsheet width={14} /> Export to .xlsx
-            </button>
+            <Link href={"/setup"}>
+                <button className="flex items-center gap-2 hover:bg-neutral-800 rounded-sm px-2 py-1 cursor-pointer w-full">
+                    <TvMinimal size={14} strokeWidth={2.5} /> Broadcast
+                </button>
+            </Link>
             
         </div>
     );
