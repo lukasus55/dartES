d36b72cd5c1f3526ae1741d084b28207c0c1113c
lukasus55 <lukas.kostyk@gmail.com>
2026-05-16 16:44:30 +0200

    Update patch notes

--- app/news/components/UpdatesContainer.tsx
+++ app/news/components/UpdatesContainer.tsx
@@ -6,7 +6,7 @@ import UpdatePreview from "./UpdatePreview";
 export default function UpdatesContainer() {
     return (
         <div className="flex flex-wrap justify-between w-full gap-6 mt-10">
-            <UpdatePreview id="v010101" title="Update 1.0.1" date="15.05.2026" />
+            <UpdatePreview id="v010101" title="Update 1.0.1" date="16.05.2026" />
             <UpdatePreview id="v010100" title="Update 1.0.0" date="12.05.2026" isMajor={true} />
         </div>
     );
