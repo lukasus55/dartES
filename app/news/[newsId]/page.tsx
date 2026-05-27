fe70d65c744352c305a1e07ce6ca75a202c2e56a
lukasus55 <lukas.kostyk@gmail.com>
2026-05-27 09:18:59 +0200

    Clear the versions mess

--- app/news/[newsId]/page.tsx
+++ app/news/[newsId]/page.tsx
@@ -1,8 +1,9 @@
 import NotFound from "@/app/not-found";
-import V010100 from "./v010100";
-import V010101 from "./v010101";
+import V010000 from "./v010000";
+import V010001 from "./v010001";
 import { Suspense } from "react";
 import Loading from "@/app/loading";
+import V010101 from "./v010101";
 
 type PropsType = {
     params: Promise<{ newsId: string }>;
@@ -16,12 +17,16 @@ export default async function News(props: PropsType) {
 
     // !!! IMPORTANT: THIS IS ONLY TEMPORARY SOLUTION. FUTURE UPDATE WILL INTRODUCE DATABSE-BASED NEWS SYSTEM !!!
 
-    if (newsId === "v010101") {
-        page = <V010101/>
+    if (newsId === "v010001") {
+        page = <V010001/>
+    }
+
+    if (newsId === "v010000") {
+        page = <V010000/>
     }
 
     if (newsId === "v010100") {
-        page = <V010100/>
+        page = <V010101/>
     }
 
     
--- app/news/components/UpdatesContainer.tsx
+++ app/news/components/UpdatesContainer.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import V010101 from "../[newsId]/v010100";
 import UpdatePreview from "./UpdatePreview";
 
 export default function UpdatesContainer() {
     return (
         <div className="flex flex-wrap justify-between w-full gap-6 mt-10">
-            <UpdatePreview id="v010101" title="Update 1.0.1" date="16.05.2026" />
-            <UpdatePreview id="v010100" title="Update 1.0.0" date="12.05.2026" isMajor={true} />
+            <UpdatePreview id="v010100" title="Update 1.1.0" date="27.05.2026" />
+            <UpdatePreview id="v010001" title="Update 1.0.1" date="16.05.2026" />
+            <UpdatePreview id="v010000" title="Update 1.0.0" date="12.05.2026" isMajor={true} />
         </div>
     );
 }
